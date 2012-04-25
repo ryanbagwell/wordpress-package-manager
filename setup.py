569ae27eca03737ed62fd07afdf80850a0dc93c6
Ryan Bagwell <ryan@ryanbagwell.com>
2012-04-25 14:40:26 -0500

    initial commit. added update, installframework, and search plugins

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from disutils import setup
+
+setup(name='Wordpress Package Manager',
+      version='1.0',
+      description='A command-line tool for installing WordPress plugins',
+      author='Ryan Bagwell',
+      author_email='ryan@ryanbagwell.com',
+      url='https://github.com/ryanbagwell',
+      packages=['distutils', 'distutils.command', 'pysvn'],
+      scripts=['bin/install'],
+     )
