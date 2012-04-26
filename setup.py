8a94d8e002d62feae958d1004c9b2cc94b427737
Ryan Bagwell <ryan@ryanbagwell.com>
2012-04-26 11:54:03 -0500

    tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='Wordpress Package Manager',
+setup(name='Wordpress-Package-Manager',
       version='0.1',
       description='A command-line tool for installing WordPress plugins',
+      long_description="A command-line tool for installing WordPress plugins in a manner similar to PIP.",
       author='Ryan Bagwell',
       author_email='ryan@ryanbagwell.com',
       url='https://github.com/ryanbagwell',
       scripts=['wpm'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: PHP',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+      ],
      )
