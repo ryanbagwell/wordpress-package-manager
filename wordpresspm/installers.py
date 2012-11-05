33b8b418d118ef523dc37754cdbed027d19ab92c
Ryan Bagwell <ryan@ryanbagwell.com>
2012-11-05 15:51:17 -0600

    refactored installframework and installplugin

--- wordpresspm/installers.py
+++ wordpresspm/installers.py
@@ -1,75 +1,48 @@
 #! /usr/bin/env python
 from optparse import OptionParser
-import sys, os, re, zipfile, shutil, urllib2, urllib, subprocess, re, tempfile, urlparse
+import sys, os, re, zipfile, shutil, urllib2, urllib, subprocess, re, tempfile, urlparse, requests
+from progressbar import ProgressBar, Percentage, Bar, FormatLabel
+from asynchttp import Http
+import subprocess
 
 class BaseInstaller(object):
     wpm_meta_path = os.path.expanduser('~/.wpm/')
     plugin_db_path = os.path.join(wpm_meta_path, 'available_plugins')
     tmp_dir = tempfile.mkdtemp()
     plugins_svn = "http://plugins.svn.wordpress.org"
- 
-
+    target_location = None
 
     def __init__(self, *args, **kwargs):
         
         for key, value in kwargs.items():
             setattr(self, key, value)
-        
-        self.set_install_location()
-        
-        
-    """
-    Sets and verifies the install path
-    """
-    def set_install_location(self, suffix=None):
-
-        if hasattr(self, self.arguments[-1]) or self.is_plugin( self.arguments[-1] ):
-            basepath = os.getcwd()
-        else:
-            basepath = os.path.abspath(self.arguments[-1])
-
-        if suffix is not None:
-            basepath = os.path.join(basepath, suffix)
-
-        try:
-            print "Setting target location"
-            os.makedirs(basepath)
-        except OSError:
-            print "Install Location already exists. Continuing."
-
-        self.target_location = basepath
 
     """
     Common method to download a file from a given url
     """        
     def download_file(self, url):
-        data = self.download_data(url)
-        local = open( os.path.join(self.tmp_dir, url.split('/')[-1]), 'w')
-        local.write(data)
-        local.close()        
-        return os.path.join(self.tmp_dir, url.split('/')[-1])
-        
-    
+        file = self.download_data(url)
+        renamed = os.path.join(os.path.dirname(file), url.split('/')[-1])
+        os.rename(file, renamed)
+        return renamed
+
+
     def download_data(self, url):
+        
+        pbar = ProgressBar(widgets=[FormatLabel('Downloading'), Percentage(), Bar()],
+            maxval=9000000).start()
+        
+        temp = tempfile.mkstemp()
+        
+        p = subprocess.Popen(['wget', '-O', temp[1], url], stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
 
-        conn = urllib2.urlopen(url)
+        while p.poll() != 0:
+            pbar.update(os.stat(temp[1]).st_size)
+            
+        pbar.finish()
 
-        data = ""
-
-        file_size_dl = 0
-        block_sz = 500
-        while True:
-            buffer = conn.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            data += buffer
-            status = "Downloaded %s bytes" % (file_size_dl)
-            status = status + chr(8)*(len(status)+2)
-            print status,
-
-        return data
+        return temp[1]
     
         
     """
@@ -80,23 +53,8 @@ class BaseInstaller(object):
         z = zipfile.ZipFile(file)
         z.extractall(self.tmp_dir)
         return os.path.join(self.tmp_dir, z.namelist()[0])
-        
-    """
-    Checks to see if the plugin is in our list of plugins
-    """
-    def is_plugin(self, plugin_name):
 
-        try:
-            file = open( self.plugin_db_path, 'w+')
-        except IOError:
-            print "Couldn't open plugin database. Do you need to run wpm setup first?"
-            return False
 
-        for line in file.readlines():
-            if line.strip() == plugin_name: return True
-
-        return False
-        
     """
     Writes new security keys to the wp-config.php file
     """
@@ -136,47 +94,38 @@ class BaseInstaller(object):
     """
     Moves the tmp install to the specified plugin location
     """
-    def move_tmp(self, plugin_name):
+    def move_tmp(self, src, dest):
+
+        if os.path.exists(os.path.abspath(dest)) and not self.overwrite:
+            print "Installation already exists"
+            return False
 
         try:
-            os.system('mv %s %s' % (os.path.join(self.tmp_dir, plugin_name), os.path.join(self.target_location, plugin_name) ))
-        except OSError:
-            print "Plugin %s already exists. Please remove it first." % plugin_name
+            shutil.rmtree(os.path.abspath(dest))
+        except:
+            pass
             
-            
-    """
-    Sets and verifies the install path
-    """
-    def set_location(self, suffix=None):
-
-        if hasattr(self, self.args[-1]) or self._is_plugin( self.args[-1] ):
-            basepath = os.getcwd()
-        else:
-            basepath = os.path.abspath(self.args[-1])
-
-        if suffix is not None:
-            basepath = os.path.join(basepath, suffix)
-
-        try:
-            print "Setting target location"
-            os.makedirs(basepath)
-        except OSError:
-            print "Install Location already exists. Continuing."
-
-        self.target_location = basepath
-            
-            
+        os.rename(src, dest)
+        
 
 class ZIPInstaller(BaseInstaller):
     
     def __init__(self, *args, **kwargs):
         super(ZIPInstaller, self).__init__(*args, **kwargs)
 
-    def install(self):
-        file = self.download_file( self.url )
-        plugin_path = self.extract(file)
-        self.move_tmp( plugin_path.rstrip('/').split('/')[-1] )
 
+    def install(self):
+
+        if os.path.exists(os.path.abspath(self.target_location)) and not self.overwrite:
+            print "Installation already exists"
+            return False
+
+        file = self.download_file(self.url)
+        extracted_path = self.extract(file)
+        
+        self.move_tmp(extracted_path, self.target_location)
+        
+        return True
 
 class GITInstaller(BaseInstaller):
     """
@@ -194,10 +143,7 @@ class SVNInstaller(BaseInstaller):
         
     def install(self):
         print "Exporting %s from %s" % (self.plugin_name, self.svn_url)
-        assert False, self.target_location
         self._run_command(['svn','export', url, self.target_location ])
-        
-
         
 """
 Download and extract the specified plugin from the
@@ -206,17 +152,17 @@ official WordPress plugin repository
 class WPInstaller(BaseInstaller):
 
     def install(self):
-        
-        if not self.is_plugin( self.plugin_name ):
-            print "Invalid plugin name. If you're sure it exists, try running update."
+
+        if os.path.exists(
+            os.path.join(self.target_location, self.plugin_name)) and not self.overwrite:
+            print "Target location exists. Aborting"
             return
-        
-        svn_url = '/'.join([self.plugins_svn,self.plugin_name.replace("\n",''),'trunk'])
+    
+        svn_url = '/'.join([self.plugins_svn, self.plugin_name.replace("\n",''), 'trunk'])
         print "Exporting %s from Wordpress SVN" % (self.plugin_name)
-        self.run_command(['svn', 'export', svn_url, self.tmp_dir+'/'+self.plugin_name ])
-        
-        self.move_tmp( self.plugin_name )
-        
+        self.run_command(['svn', 'export', svn_url, 
+            os.path.join(self.target_location, self.plugin_name)])
+
 """
 Creates the wpm meta directory, and downlods the list of available plugins.
 """
@@ -241,7 +187,3 @@ class DBInstaller(BaseInstaller):
         local.write("\r\n".join(plugins))
         
         local.close()
-            
-
-
-    
