c9bd0a2c4eb5adebf329f76cb775623753999d13
Ryan Bagwell <ryan@ryanbagwell.com>
2014-01-15 14:23:49 -0600

    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from distutils.command.install_data import install_data
 
 setup(name='Wordpress-Package-Manager',
-      version='0.7.6',
+      version='0.7.7',
       description='A command-line tool for installing WordPress plugins',
       long_description="A command-line tool for installing WordPress plugins in a manner similar to PIP.",
       author='Ryan Bagwell',
