a5e0c48a10dec96769982f4a44a5c6a61e7723b4
Ryan Bagwell <ryan@ryanbagwell.com>
2014-05-08 13:12:03 -0500

    pep8

--- wordpresspm/installers.py
+++ wordpresspm/installers.py
@@ -1,7 +1,13 @@
 #! /usr/bin/env python
-import sys, os, re, zipfile, shutil, urllib2, urllib, subprocess, re, tempfile, urlparse
-from progressbar import ProgressBar, Percentage, Bar, FormatLabel
+import os
+import re
+import zipfile
+import shutil
+import urllib2
 import subprocess
+import tempfile
+from progressbar import ProgressBar, Percentage, Bar, FormatLabel
+
 
 class BaseInstaller(object):
     wpm_meta_path = os.path.expanduser('~/.wpm/')
@@ -19,19 +25,19 @@ class BaseInstaller(object):
     """
     Common method to download a file from a given url
     """
+
     def download_file(self, url):
         return self.download_data(url)
-
 
     def download_data(self, url):
 
         pbar = ProgressBar(widgets=[FormatLabel('Downloading'), Percentage(), Bar()],
-            maxval=9000000).start()
+                           maxval=9000000).start()
 
         temp = tempfile.mkstemp()
 
         p = subprocess.Popen(['wget', '-O', temp[1], url], stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+                             stderr=subprocess.PIPE)
 
         while p.poll() != 0:
             pbar.update(os.stat(temp[1]).st_size)
@@ -40,23 +46,23 @@ class BaseInstaller(object):
 
         return temp[1]
 
-
     """
     Unzip a plugin in .zip format
     """
+
     def extract(self, file):
         print "\r\nUnzipping ..."
         z = zipfile.ZipFile(file)
         z.extractall(self.tmp_dir)
         return z.namelist()
 
-        #return os.path.join(self.tmp_dir, z.namelist())
-
+        # return os.path.join(self.tmp_dir, z.namelist())
 
     """
     Writes new security keys to the wp-config.php file
     """
-    def set_security_keys(self, config_file = None):
+
+    def set_security_keys(self, config_file=None):
 
         if not config_file:
             return
@@ -64,12 +70,13 @@ class BaseInstaller(object):
         print "Generating new security keys"
 
         try:
-            keys = urllib2.urlopen('https://api.wordpress.org/secret-key/1.1/salt/').readlines()
+            keys = urllib2.urlopen(
+                'https://api.wordpress.org/secret-key/1.1/salt/').readlines()
         except:
             print "Couldn't generate security keys from WordPress API. You'll have to set them yourself."
-            return;
+            return
 
-        config_file = open(self.target_location+'/wp-config.php','r+')
+        config_file = open(self.target_location + '/wp-config.php', 'r+')
         lines = config_file.readlines()
 
         for i in range(len(lines)):
@@ -84,14 +91,17 @@ class BaseInstaller(object):
     """
     Runs a command
     """
+
     def run_command(self, cmd_list):
-        proc = subprocess.Popen(cmd_list,stdout=subprocess.PIPE)
+        proc = subprocess.Popen(cmd_list, stdout=subprocess.PIPE)
         for line in proc.stdout:
-            if line is not "\r\n": print line
+            if line is not "\r\n":
+                print line
 
     """
     Moves the tmp install to the specified plugin location
     """
+
     def move_tmp(self, src, dest):
 
         if os.path.exists(os.path.abspath(dest)) and not self.overwrite:
@@ -111,7 +121,6 @@ class ZIPInstaller(BaseInstaller):
     def __init__(self, *args, **kwargs):
         super(ZIPInstaller, self).__init__(*args, **kwargs)
 
-
     def install(self):
 
         target_location = os.path.join(self.target_location, self.plugin_name)
@@ -124,44 +133,54 @@ class ZIPInstaller(BaseInstaller):
 
         return True
 
+
 class GITInstaller(BaseInstaller):
+
     """
     Clones a git repository
     """
+
     def install(self):
         print "Cloning %s from %s" % (self.plugin_name, self.url)
-        self.run_command(['git','clone', self.url, os.path.join(self.target_location, self.plugin_name) ])
+        self.run_command(
+            ['git', 'clone', self.url, os.path.join(self.target_location, self.plugin_name)])
 
 
 """
 Exports from the trunk of an svn repository
 """
+
+
 class SVNInstaller(BaseInstaller):
 
     def install(self):
         print "Exporting %s from %s" % (self.plugin_name, self.svn_url)
-        self._run_command(['svn','export', url, self.target_location ])
+        self._run_command(['svn', 'export', url, self.target_location])
 
 """
 Download and extract the specified plugin from the
 official WordPress plugin repository
 """
+
+
 class WPInstaller(BaseInstaller):
 
     def install(self):
 
         if os.path.exists(
-            os.path.join(self.target_location, self.plugin_name)) and not self.overwrite:
+                os.path.join(self.target_location, self.plugin_name)) and not self.overwrite:
             print "Target location exists. Aborting"
             return
 
-        svn_url = '/'.join([self.plugins_svn, self.plugin_name.replace("\n",''), 'trunk'])
+        svn_url = '/'.join([self.plugins_svn,
+                            self.plugin_name.replace("\n", ''), 'trunk'])
         print "Exporting %s from Wordpress SVN" % (self.plugin_name)
         self.run_command(['svn', 'export', svn_url,
-            os.path.join(self.target_location, self.plugin_name)])
+                          os.path.join(self.target_location, self.plugin_name)])
 
 
 class FrameworkInstaller(ZIPInstaller):
+
     """ Downlads and installs the WP framework """
 
     def install(self):
@@ -170,7 +189,8 @@ class FrameworkInstaller(ZIPInstaller):
 
         extracted_files = self.extract(file)
 
-        self.move_tmp(os.path.join(self.tmp_dir, 'wordpress'), self.target_location)
+        self.move_tmp(
+            os.path.join(self.tmp_dir, 'wordpress'), self.target_location)
 
         return True
 
@@ -178,6 +198,8 @@ class FrameworkInstaller(ZIPInstaller):
 """
 Creates the wpm meta directory, and downlods the list of available plugins.
 """
+
+
 class DBInstaller(BaseInstaller):
 
     def install(self):
@@ -193,7 +215,7 @@ class DBInstaller(BaseInstaller):
 
         existing_contents = list(local.read())
 
-        plugins = re.findall('">(.*)/</a>',data)
+        plugins = re.findall('">(.*)/</a>', data)
 
         local.seek(0)
         local.write("\r\n".join(plugins))
