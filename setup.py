19d943be8ab71d3ab72fe51e5aef674bb9355fa1
Ryan Bagwell <ryan@ryanbagwell.com>
2014-05-08 15:19:24 -0500

    working on compilation

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from distutils.command.install_data import install_data
-from cx_Freeze import setup, Executable
 
 setup(name='Wordpress-Package-Manager',
       version='0.8.2',
@@ -11,8 +10,7 @@ setup(name='Wordpress-Package-Manager',
       author_email='ryan@ryanbagwell.com',
       url='https://github.com/ryanbagwell',
       py_modules=['wordpresspm.installers', 'wordpresspm.wpm'],
-      executables = [Executable("wordpresspm/wpm.py", targetDir='bin')],
-      #scripts=['bin/wpm'],
+      scripts=['bin/wpm'],
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -23,7 +21,5 @@ setup(name='Wordpress-Package-Manager',
         'License :: OSI Approved :: Python Software Foundation License',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
-      ],
-      install_requires=[
-        'cx_freeze',
-      ])
+      ]
+    )
